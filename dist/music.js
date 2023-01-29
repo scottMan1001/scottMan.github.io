cc8b98e43453ecfb645a5dbd3955d8a9711d1467
ScottMan1001 <3141996514@qq.com>
2023-01-29 12:49:54 +0800

    Site updated: 2023-01-29 12:49:44

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+  container: document.getElementById("aplayer"),
+  fixed: true,
+  autoplay: false,
+  audio: [
+    {
+      name: "溯（钢琴版）",
+      artist: "柳轻颂",
+      url: "http://music.163.com/song/media/outer/url?id=1433584979.mp3",
+      cover:
+        "http://p1.music.126.net/qX7knUIlpmbJ34UBzCCS6w==/109951164802366812.jpg?param=130y130",
+    },
+
+    {
+      name: "スパークル（火花）——《你的名字。》插曲",
+      artist: "ゆう十",
+      url: "http://music.163.com/song/media/outer/url?id=1984045303.mp3",
+      cover:
+        "http://p2.music.126.net/SiTInUUumsPdSdndv1MCyw==/109951167903888878.jpg?param=130y130",
+    },
+    {
+      name: "euphoria",
+      artist: " ",
+      url: "http://music.163.com/song/media/outer/url?id=1814327255.mp3",
+      cover: "/images/susan1.jpg",
+    },
+    {
+      name: "Friend",
+      artist: "Kozoro",
+      url: "http://music.163.com/song/media/outer/url?id=33004707.mp3",
+      cover:
+        "http://p2.music.126.net/FR7zKYB8ujNRoWQuGCHbGg==/109951163288692527.jpg?param=130y130",
+    },
+    {
+      name: "One Last Time",
+      artist: "廖泽蓝",
+      url: "http://music.163.com/song/media/outer/url?id=1870680152.mp3",
+      cover:
+        "http://p1.music.126.net/Y_Io_ln1-ujiL9eFdyQvmQ==/109951166291345875.jpg?param=130y130",
+    },
+    {
+      name: "都对也都错",
+      artist: "张惠妹",
+      url: "http://music.163.com/song/media/outer/url?id=28754096.mp3",
+      cover:
+        "http://p2.music.126.net/R3QLz43nYuEGlK4ucAaNAg==/109951167256508748.jpg?param=130y130",
+    },
+  ],
+});
